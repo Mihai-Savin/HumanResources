d14a0270e14408b8cf8cce6ca317114de13fa194
Mihai Savin <savin.mihai@gmail.com>
2015-11-23 15:30:08 +0200

    Created skeleton of app and Employee class.

---
+++ src/Employee.java
@@ -0,0 +1,9 @@
+
+public class Employee {
+	private String firstName;
+	private String lastName;
+	private String dateOfBirth;
+	private char gender;
+	
+
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		handleEmployees();
+	}
+
+	private static void handleEmployees() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
